fd3c1277ea06d30b4d014388930c6415d0edf082
Josh <lowranceworks@gmail.com>
2021-01-25 14:07:58 -0600

    init commit

---
+++ provider.tf
@@ -0,0 +1,25 @@
+# import variables from terraform.tfvars
+variable "ibmcloud_api_key" {}
+variable "iaas_classic_username" {}
+variable "iaas_classic_api_key" {}
+
+# import IBM provider
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "~> 1.20.0"
+    }
+  }
+}
+
+# configure IBM provider
+provider "ibm" {
+  ibmcloud_api_key          = var.ibmcloud_api_key
+  iaas_classic_username     = var.iaas_classic_username
+  iaas_classic_api_key      = var.iaas_classic_api_key
+  generation                = 2
+  region                    = "us-south"
+  # iaas_classic_username = var.iaas_classic_username
+  # iaas_classic_api_key  = var.iaas_classic_api_key
+}
---
+++ vpc.tf
@@ -0,0 +1,4 @@
+# Create a VPC
+resource "ibm_is_vpc" "vpc" {
+  name = "pratice-vpc"
+}
